98a3e8c6100dedc61e639bd935625d06f2b390cd
Mohammed Taha Alibhai <mot1922092@sicsr.ac.in>
2022-06-16 23:07:11 +0300

    feat: created Routes and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
+import "./App.css"
+import { Navbar,Footer } from './components'
+import { ExerciseDetail, Home } from './pages'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{ width:{xl:"148px"}}}  m='auto'>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 

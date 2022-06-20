c9c1db5d65fda5fbc681134ea2c1fcee6b77b6a5
Mohammed Taha Alibhai <mot1922092@sicsr.ac.in>
2022-06-20 17:34:14 +0300

    feat: capitalized exercise name

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -3,7 +3,6 @@ import { Typography, Stack, Button } from '@mui/material'
 import BodyPartImage from '../assets/icons/body-part.png'
 import TargetImage from '../assets/icons/target.png'
 import EquipmentImage from '../assets/icons/equipment.png'
-import { borderRadius } from '@mui/system'
 
 
 const Detail = ({exerciseDetail}) => {
@@ -58,7 +57,7 @@ const Detail = ({exerciseDetail}) => {
           }
         }}>
 
-          <Typography variant="h3">
+          <Typography textTransform='capitalize' variant="h4">
             {name}
           </Typography>
           

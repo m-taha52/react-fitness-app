7ee19e61932d965562cca55c00fcb35075c2564c
Mohammed Taha Alibhai <mot1922092@sicsr.ac.in>
2022-06-20 17:36:21 +0300

    feat: completed Exercise Detail Page

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,11 +1,14 @@
 import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import { Box } from '@mui/material'
-import {exerciseOptions, fetchData} from "../utils/fetchData"
+import {exerciseOptions, fetchData, youtubeOptions} from "../utils/fetchData"
 import { Detail, ExerciseVideos, SimilarExercises } from '../components'
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({})
+  const [exerciseVideos, setExerciseVideos] = useState([])
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([])
+  const [equipmentExercises, setEquipmentExercises] = useState([])
   const { id } = useParams()
 
   useEffect(() => {
@@ -13,9 +16,20 @@ const ExerciseDetail = () => {
     const fetchExercisesData = async () => {
       const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
       const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com/'
+      const quotesUrl = 'https://bodybuilding-quotes1.p.rapidapi.com/single-quote'
 
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions)
       setExerciseDetail(exerciseDetailData)
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions)
+      setExerciseVideos(exerciseVideosData.contents)
+
+      const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions) 
+      setTargetMuscleExercises(targetMuscleExercisesData)
+     
+      const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions) 
+      setEquipmentExercises(equipmentExercisesData)
+
 
     }
 
@@ -26,9 +40,9 @@ const ExerciseDetail = () => {
   return (
 
     <Box>
-      <Detail exerciseDetail={exerciseDetail}/>
-      <ExerciseVideos />
-      <SimilarExercises />
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
     </Box>
   )
 }
